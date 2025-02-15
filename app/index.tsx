ff0f576929015aaeb9a2e2662d24a464217d19d7
erlendtyrmi <s132962@dtu.dk>
2025-02-15 13:40:08 +0100

    added native wind

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,14 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import "./../global.css";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">
-        Welcome to Expo + Tailwind CSS
-      </Text>
-      <Text className="text-3xl font-pblack">
-        Welcome to Expo + Tailwind CSS
-      </Text>
-      <Text className="text-3xl font-pblack">
-        Welcome to Expo + Tailwind CSS
-      </Text>
-      <Text className="text-3xl font-pblack">
-        Welcome to Expo + Tailwind CSS
-      </Text>
-      <Text className="text-3xl font-pblack">
-        Welcome to Expo + Tailwind CSS
-      </Text>
-      <Text className="text-3xl font-pblack">
-        Welcome to Expo + Tailwind CSS
-      </Text>
-      <Text className="text-3xl font-pblack">Aora for daura</Text>
+      <Text className="text-3xl font-pblack">Allora</Text>
       <StatusBar style="auto" />
-      <Link href="/home">Go to Høm</Link>
+      <Link href="/home">Go home</Link>
     </View>
   );
 }
